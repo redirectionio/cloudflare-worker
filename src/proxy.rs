575d4cba308dca343d2669afd37fb7764637ffff
Joel Wurtz <jwurtz@jolicode.com>
2023-03-23 15:08:33 +0100

    feat(proxy): support split cookies

--- src/proxy.rs
+++ src/proxy.rs
@@ -19,7 +19,14 @@ pub async fn proxy(worker_request: WorkerRequest, action: &mut Action, add_rules
 
     for (name, value) in response.headers() {
         if name.to_lowercase() == "set-cookie" {
-            continue; // @TODO Split set cookie
+            let values = split_set_cookie(value.as_str());
+
+            for value in values {
+                headers.push(Header {
+                    name: name.to_string(),
+                    value,
+                });
+            }
         } else {
             headers.push(Header { name, value });
         }
@@ -50,5 +57,56 @@ pub async fn proxy(worker_request: WorkerRequest, action: &mut Action, add_rules
 
             Ok((response_filtered, filtered_headers, backend_status_code))
         }
+    }
+}
+
+fn split_set_cookie(set_cookie: &str) -> Vec<String> {
+    let mut cookies = Vec::new();
+
+    set_cookie.split(',').for_each(|cookie| {
+        let pos_equal = cookie.find('=');
+        let pos_semi_colon = cookie.find(';');
+
+        let append_to_last = match pos_equal {
+            None => true,
+            Some(pos_equal) => match pos_semi_colon {
+                None => false,
+                Some(pos_semi_colon) => pos_equal > pos_semi_colon,
+            },
+        };
+
+        if append_to_last {
+            match cookies.last_mut() {
+                None => cookies.push(cookie.trim().to_string()),
+                Some(last_cookie) => {
+                    last_cookie.push(',');
+                    last_cookie.push_str(cookie);
+                }
+            }
+        } else {
+            cookies.push(cookie.trim().to_string());
+        }
+    });
+
+    cookies
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    pub fn test_split_cookies() {
+        let cookie_str = "sessionid=6ky4pkr7qoi4me7rwleyvxjove25huef, cid=70125eaa-399a-41b2-b235-8a5092042dba; expires=Thu, 04-Jun-2020 12:17:56 GMT; Max-Age=63072000; Path=/; HttpOnly; Secure, client_id=70125eaa-399a-41b2-b235-8a5092042dba; Max-Age=63072000; Path=/; expires=Thu, 04-Jun-2020 12:17:56 GMT";
+        let cookies = super::split_set_cookie(cookie_str);
+
+        assert_eq!(cookies.len(), 3);
+        assert_eq!(cookies[0], "sessionid=6ky4pkr7qoi4me7rwleyvxjove25huef");
+        assert_eq!(
+            cookies[1],
+            "cid=70125eaa-399a-41b2-b235-8a5092042dba; expires=Thu, 04-Jun-2020 12:17:56 GMT; Max-Age=63072000; Path=/; HttpOnly; Secure"
+        );
+        assert_eq!(
+            cookies[2],
+            "client_id=70125eaa-399a-41b2-b235-8a5092042dba; Max-Age=63072000; Path=/; expires=Thu, 04-Jun-2020 12:17:56 GMT"
+        );
     }
 }
